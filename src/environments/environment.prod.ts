7c8ca8cd6eb768a10982e7f26e41e7488a7ceba6
Wesley Chitwood <wesley.chitwood1@gmail.com>
2018-03-16 14:40:48 -0400

    update production environment with firebase config snippets

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAPDTgi9DGlKH2suHBoafsgy-3Otbyd2To",
+    authDomain: "organic-shop-18cd6.firebaseapp.com",
+    databaseURL: "https://organic-shop-18cd6.firebaseio.com",
+    projectId: "organic-shop-18cd6",
+    storageBucket: "organic-shop-18cd6.appspot.com",
+    messagingSenderId: "124059202174"
+  }
 };
