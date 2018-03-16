179f4b4536c01baebc5f8f0bb1b2d4aa23cfa7f2
Wesley Chitwood <wesley.chitwood1@gmail.com>
2018-03-16 02:36:11 -0400

    install and setup firebase, firebase module, firebase module database

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAPDTgi9DGlKH2suHBoafsgy-3Otbyd2To",
+    authDomain: "organic-shop-18cd6.firebaseapp.com",
+    databaseURL: "https://organic-shop-18cd6.firebaseio.com",
+    projectId: "organic-shop-18cd6",
+    storageBucket: "organic-shop-18cd6.appspot.com",
+    messagingSenderId: "124059202174"
+  }
 };
